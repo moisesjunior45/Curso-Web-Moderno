07511ef6857129ea48b640849c83f94eb4b0abdd
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2022-08-22 16:37:52 -0300

    Update listaExercicios.js

--- Lista de Exercícios - JS/listaExercicios.js
+++ Lista de Exercícios - JS/listaExercicios.js
@@ -29,4 +29,50 @@ function calcularSalario(quantidadeHorasTrabalhadas, valorHora) {
     return `Salário igual a R$${salario}`;
 }
 
-console.log(calcularSalario(150, 50.5));
+console.log(calcularSalario(150, 40.5));
+
+/*Ex4 - Crie uma função que recebe um número (de 1 a 12) e retorne o mês correspondente como uma string. 
+Por exemplo, se a entrada for 2, a função deverá retornar "fevereiro",pois este é o 2 mês*/
+
+function nomeDoMes(mes) {
+    if (mes === 1) {
+        console.log("janeiro");
+    } else if (mes === 2) {
+        console.log("fevereiro");
+    } else if (mes === 3) {
+        console.log("março");
+    } else if (mes === 4) {
+        console.log("abril");
+    } else if (mes === 5) {
+        console.log("maio");
+    } else if (mes === 6) {
+        console.log("junho");
+    } else if (mes === 7) {
+        console.log("julho");
+    } else if (mes === 8) {
+        console.log("agosto");
+    } else if (mes === 9) {
+        console.log("setembro");
+    } else if (mes === 10) {
+        console.log("outubro");
+    } else if (mes === 11) {
+        console.log("novembro");
+    } else if (mes === 12) {
+        console.log("dezembro");
+    }
+}
+
+nomeDoMes(1);
+nomeDoMes(4);
+
+
+// Ex5 - Crie uma função que receba dois números e retorne se o primeiro é maior ou igual ao segundo.
+
+function maiorOuIgual(num1, num2) {
+    if(typeof num1 != typeof num2) return false;
+    return num1 >= num2;
+}
+
+console.log(maiorOuIgual(0, 0));
+console.log(maiorOuIgual(0, "0"));
+console.log(maiorOuIgual(5, 1));
