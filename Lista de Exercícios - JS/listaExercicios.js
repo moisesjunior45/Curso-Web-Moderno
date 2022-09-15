f5ec585d6e911052177aeaaf4ac28d1a5c774642
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2022-09-15 11:41:32 -0300

    Update listaExercicios.js

--- Lista de Exercícios - JS/listaExercicios.js
+++ Lista de Exercícios - JS/listaExercicios.js
@@ -277,3 +277,32 @@ function receberSomenteOsParesDeIndicesPares(numeros) {
 }*/
 console.log(receberSomenteOsParesDeIndicesPares([1, 2, 3, 4]));
 console.log(receberSomenteOsParesDeIndicesPares([10, 70, 22, 43]));
+
+/*  Ex16 - A fim de manter o calendário anual ajustado com o movimento de translação da Terra, criou-se os anos bissextos,
+que têm 366 dias em vez dos 365 presentes nos anos normais.
+Para determinar se um ano é bissexto, é necessário saber se ele é multiplo de 4. Não pode ser múltiplo de 100,
+exceto se for também múltiplo de 400.
+Com isso em mente, desenvolva uma função que recebe um número correspondente a um ano e retorna se ele é
+bissexto ou não.*/
+
+function checarAnoBissexto(ano) {
+    const divisivelPorQuatro = ano %  4 === 0;
+    const diviselPorCem = ano % 100 === 0;
+    const diviselPorQuatrocentos = ano % 400 === 0;
+    if((divisivelPorQuatro && !diviselPorCem) || diviselPorQuatrocentos) {
+        return "É bissexto";
+    } else {
+        return "Não é bissexto"
+    }
+}
+
+// checa-se indiretamente, verificando se o mês de fevereiro do dado ano tem 29 dias
+/*function checarAnoBissexto(ano) {
+    return new Date(ano, 1, 29).getDate() === 29;
+    }*/
+    
+
+console.log(checarAnoBissexto(2020));
+console.log(checarAnoBissexto(2100));
+
+// Ex17 - Escreva um array que receba um array de números e retornará a soma de todos os números desse array.
