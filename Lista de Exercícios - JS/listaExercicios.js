68b56de2ccd1dc9eb5aae42a78d861f1d62bf302
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2022-09-21 16:22:53 -0300

    Editando arquivo

--- Lista de Exercícios - JS/listaExercicios.js
+++ Lista de Exercícios - JS/listaExercicios.js
@@ -420,3 +420,48 @@ function menorNumero(array) {
 
 console.log(menorNumero([10, 5, 35, 65]));
 console.log(menorNumero([5, -15, 50, 3]));
+
+/*Ex22 - Desenvolva uma função que receba como parâmetro um número de 1 a 10. Internamente, na função, será gerado
+um número aleatório de 1 a 10. A função deverá retornar se o parâmetro de entrada foi igual ao número sorteado
+internamente. Se o valor fornecido foi o sorteado, retorne "Parabéns! O número sorteado foi o X". Se não for igual,
+retorne "Que pena! O número sorteado foi o X". X é o número que foi sorteado.*/
+
+function funcaoDaSorte(numero) {
+    const numMinimo = 1;
+    const numMaximo = 10;
+    const numeroAleoatorio = Math.floor(Math.random() * (numMaximo - numMinimo + 1) + numMinimo);
+
+    return (numero === numeroAleoatorio) ? `Parabéns! O número sorteado foi o ${numeroAleoatorio}.` :
+        `Que pena! O número sorteado foi o ${numeroAleoatorio}.`
+}
+
+console.log(funcaoDaSorte(10));
+console.log(funcaoDaSorte(5));
+console.log(funcaoDaSorte(5));
+
+// Ex23 - Criar uma função que receba uma string como parâmetro e conte quantas palavras tem nela.
+
+function contarPalavras(frase) {
+    const palavras = frase.split(" ");
+    return palavras.length;
+}
+
+console.log(contarPalavras("Sou uma frase"));
+console.log(contarPalavras("Me divirto aprendendo a programar"));
+
+/*Ex24 - Desenvolva uma função que recebe um caractere e uma string como parâmetros e retorne a quantidade de vezes
+que o caractere se repete na string. A função deverá ser case-sensitive, ou seja, irá diferenciar maiúsculas de
+minúsculas.*/
+
+function contarCaractere(caractere, frase) {
+    let contador = 0;
+
+    for (let i = 0; i < frase.length; i++) {
+        if (frase.charAt(i) === caractere)
+            contador++;
+    }
+    return contador;
+}
+
+console.log(contarCaractere("r", "A sorte favorece os audazes"));
+console.log(contarCaractere("c", "Conhece-te a ti mesmo"));
