5cfeed40ac96d537cfc4a7001fb7cea86035be48
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2022-10-12 22:40:38 -0300

    Editando arquivos

--- Integrando Html,CSS e JS/dom/js/flappy.js
+++ Integrando Html,CSS e JS/dom/js/flappy.js
@@ -15,11 +15,11 @@ function Barreira(reversa = false) {
     this.setAltura = altura => corpo.style.height = `${altura}px`;
 }
 
-// const b = new Barreira(true);
-// b.setAltura(300);
-// document.querySelector('[wm-flappy]').appendChild(b.elemento);
+// const b = new Barreira(true)
+// b.setAltura(300)
+// document.querySelector('[wm-flappy]').appendChild(b.elemento)
 
- function parDeBarreiras(altura, abertura, x) {
+function ParDeBarreiras(altura, abertura, x) {
     this.elemento = novoElemento('div', 'par-de-barreiras');
 
     this.superior = new Barreira(true);
@@ -36,12 +36,77 @@ function Barreira(reversa = false) {
     }
 
     this.getX = () => parseInt(this.elemento.style.left.split('px')[0]);
-    this.setX  = x => this.elemento .style.left = `${x}px`;
-    this.getLargura = () => this.elemento.clienteWidth;
+    this.setX = x => this.elemento.style.left = `${x}px`;
+    this.getLargura = () => this.elemento.clientWidth;
 
     this.sortearAbertura();
     this.setX(x);
- }
+}
 
- const b = new parDeBarreiras(700, 250, 800);
-document.querySelector('[wm-flappy]').appendChild(b.elemento);
+// const b = new ParDeBarreiras(700, 200, 800)
+// document.querySelector('[wm-flappy]').appendChild(b.elemento)
+
+function Barreiras(altura, largura, abertura, espaco, notificarPonto) {
+    this.pares = [
+        new ParDeBarreiras(altura, abertura, largura),
+        new ParDeBarreiras(altura, abertura, largura + espaco),
+        new ParDeBarreiras(altura, abertura, largura + espaco * 2),
+        new ParDeBarreiras(altura, abertura, largura + espaco * 3)
+    ];
+
+    const deslocamento = 3;
+    this.animar = () => {
+        this.pares.forEach(par => {
+            par.setX(par.getX() - deslocamento);
+
+            // quando o elemento sair da área do jogo
+            if (par.getX() < -par.getLargura()) {
+                par.setX(par.getX() + espaco * this.pares.length);
+                par.sortearAbertura();
+            }
+
+            const meio = largura / 2;
+            const cruzouOMeio = par.getX() + deslocamento >= meio
+                && par.getX() < meio;
+            if (cruzouOMeio) notificarPonto();
+        })
+    }
+}
+
+function Passaro(alturaJogo) {
+    let voando = false;
+
+    this.elemento = novoElemento('img', 'passaro');
+    this.elemento.src = 'imgs/passaro.png';
+
+    this.getY = () => parseInt(this.elemento.style.bottom.split('px')[0]);
+    this.setY = y => this.elemento.style.bottom = `${y}px`;
+
+    window.onkeydown = e => voando = true;
+    window.onkeyup = e => voando = false;
+
+    this.animar = () => {
+        const novoY = this.getY() + (voando ? 8 : -5);
+        const alturaMaxima = alturaJogo - this.elemento.clientHeight;
+
+        if (novoY <= 0) {
+            this.setY(0);
+        } else if (novoY >= alturaMaxima) {
+            this.setY(alturaMaxima);
+        } else {
+            this.setY(novoY);
+        }
+    }
+
+    this.setY(alturaJogo / 2);
+}
+
+const barreiras = new Barreiras(700, 1200, 200, 400);
+const passaro = new Passaro(700);
+const areaDoJogo = document.querySelector('[wm-flappy]');
+areaDoJogo.appendChild(passaro.elemento);
+barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento));
+setInterval(() => {
+    barreiras.animar();
+    passaro.animar();
+}, 20);
