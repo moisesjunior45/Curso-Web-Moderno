9fff0d0350e26351428d1cb9c6ca3ce15188ca0d
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2023-01-23 21:57:03 -0300

    Editando arquivos

--- bootstrap/exercicios/js/navegacao.js
+++ bootstrap/exercicios/js/navegacao.js
@@ -3,7 +3,7 @@
         if (!hash) return
 
         const link = document.querySelector(`[wm-link='${hash}']`)
-        if (!link) return
+        if(!link) return
 
         const destino = document.querySelector('[wm-link-destino]')
 
@@ -12,7 +12,6 @@
             .then(resp => resp.text())
             .then(html => {
                 destino.innerHTML = html
-                eval(html.match(/\<script\>([\s\S]*)\<\/script\>/)[1])
             })
     }
 
@@ -33,7 +32,7 @@
     }
 
     window.onhashchange = e => navegarViaAjax(location.hash)
-
+    
     configurarLinks()
     navegacaoInicial()
 })()
