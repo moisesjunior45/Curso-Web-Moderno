5426fc4f8507a52f666326989f81e1767fe85610
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2024-01-26 11:26:54 -0300

    Concluindo aula "Processando JavaScript com Gulp"

--- gulp/javascript/gulpfile.js
+++ gulp/javascript/gulpfile.js
@@ -5,15 +5,19 @@ const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
 
 function padrao(cb) {
-    gulp.src('src/**/*.js')
+    return gulp.src('src/**/*.js')
         .pipe(babel({
             comments: false,
             presets: ["env"]
         }))
         .pipe(uglify())
+        .on('error', err => console.log(err))
         .pipe(concat('codigo.min.js'))
         .pipe(gulp.dest('build'))
+}
 
+function fim(cb) {
+    console.log('Fim!')
     return cb()
 }
 
