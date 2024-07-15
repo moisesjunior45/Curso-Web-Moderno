c7b518ce747e4b33c24e0f7560e5544a7511edfd
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2024-07-15 18:03:39 -0300

    Múltiplos componentes e Componente e classe

---
+++ react/exercicios/src/components/Multiplos.jsx
@@ -0,0 +1,8 @@
+// Componente Boa tarde
+const BoaTarde = (props) => <h1>Boa tarde {props.nome}</h1>;
+
+// Componente Boa noite
+const BoaNoite = (props) => <h1>Boa noite {props.nome}</h1>;
+
+export { BoaTarde, BoaNoite };
+// export default { BoaTarde, BoaNoite };
--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Primeiro from './components/Primeiro';
-import BomDia from './components/BomDia';
+// Componentes
+// import Primeiro from './components/Primeiro';
+// import BomDia from './components/BomDia';
+// import Multi, { BoaNoite } from './components/Multiplos';
+import Saudacao from './components/Saudacao';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Primeiro />
+    {/* <Primeiro />
     <BomDia nome="Guilherme" />
+    <Multi.BoaTarde nome="Ana" />
+    <BoaNoite nome="Bia" /> */}
+    <Saudacao tipo="Bom dia" nome="Guilherme" />
   </React.StrictMode>
 );
